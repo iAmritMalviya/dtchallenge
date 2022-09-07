7acee6b559093c18abcec6c798c07e150b32d8f9
iAmritMalviya <malviyaamrat42@gmail.com>
2022-09-07 13:50:45 +0530

    image uploading part has been completed

--- main.js
+++ main.js
@@ -3,20 +3,58 @@ var ObjectID = require("mongodb").ObjectID;
 var MongoClient = require("mongodb").MongoClient;
 var fs = require("fs");
 var multer = require("multer");
+var path = require("path");
+
+// fileUpload = require('express-fileupload')
 ejs = require("ejs");
-path = require('path')
+path = require("path");
 bodyParser = require("body-parser");
 const app = express();
-
-
-
+app.use("/images", express.static("images"));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+var upload = multer({ storage: storage });
+
+// const imageStorage = multer.diskStorage({
+//   // Destination to store image
+//   destination: '/images',
+//     filename: (req, file, cb) => {
+//         cb(null, file.fieldname + '_' + Date.now()
+//            + path.extname(file.originalname))
+//           // file.fieldname is name of the field (image)
+//           // path.extname get the uploaded file extension
+//   }
+// });
+
+// const imageUpload = multer({
+//   storage: imageStorage,
+//   limits: {
+//     fileSize: 1000000 // 1000000 Bytes = 1 MB
+//   },
+//   fileFilter(req, file, cb) {
+//     if (!file.originalname.match(/\.(png|jpg)$/)) {
+//        // upload only png and jpg format
+//        return cb(new Error('Please upload a Image'))
+//      }
+//    cb(undefined, true)
+// }
+// })
+
+// app.use(fileUpload());
 let uid;
 let dbo;
 let url = "mongodb://localhost:27017/";
 app.use(express.static("public"));
+
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
 
@@ -92,44 +130,41 @@ app
         });
     }
   })
-  .post( function (req, res) {
+  .post(upload.single("image"), function (req, res) {
     console.log(req.file);
-    
-     
-      // if (req.file == null) {
-      //   res.send("you didnnot upload a picture ");
-      // }
-    
+    // res.send(req.file)
 
-        // var newImg = fs.readFileSync(req.file.path);
-        // var encImg = newImg.toString("base64");
-        var newItem = {
-          type: req.body.type,
-          eventName: req.body.eventName,
-          tagLine: req.body.tagLine,
-          schedule: req.body.schedule,
-          description: req.body.description,
-          moderator: req.body.moderator,
-          category: req.body.category,
-          subcategory: req.body.subcategory,
-          rigorRank: req.body.rigorRank,
-          attendees: req.body.attendees,
-          // size: req.file.size,
-          // img: Buffer(encImg, "base64"),
-          
-          // contentType: req.file.mimetype,
-        };
+    if (req.file == null) {
+      res.send("you didnnot upload a picture ");
+    } else {
+      var newImg = fs.readFileSync(req.file.path);
+      var encImg = newImg.toString("base64");
+      var newItem = {
+        type: req.body.type,
+        eventName: req.body.eventName,
+        tagLine: req.body.tagLine,
+        schedule: req.body.schedule,
+        description: req.body.description,
+        moderator: req.body.moderator,
+        category: req.body.category,
+        subcategory: req.body.subcategory,
+        rigorRank: req.body.rigorRank,
+        attendees: req.body.attendees,
+        size: req.file.size,
+        img: Buffer.from(encImg, "base64"),
 
+        contentType: req.file.mimetype,
+      };
+      console.log(newItem);
 
-        dbo.collection("events").insertOne(newItem, function (err, result) {
-          if (err) {
-            return err;
-          }
-          console.log("data inserted");
-          res.json(result);
-        });
-      
-    
+      dbo.collection("events").insertOne(newItem, function (err, result) {
+        if (err) {
+          return err;
+        }
+        console.log("data inserted");
+        res.json(result);
+      });
+    }
   });
 app
   .route("/events/:id")
